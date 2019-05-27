091bafada7c52db93a40f84e46bab374042e7deb
2006.gbm@gmail.com <GBM-2006>
2019-05-27 17:22:08 -0500

    20190527 Commit inicial

---
+++ 01-JavaHello/src/com/srccodes/example/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.srccodes.example;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World");
+        System.out.println("How are You?");
+        System.out.println("Proof Stage");
+        System.out.println("Proof GUI and git status");
+	}
+
+}
