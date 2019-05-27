94fba122f8c3b26c3f6c9926a435b2ace6c8b22a
2006.gbm@gmail.com <GBM-2006>
2019-05-27 17:45:38 -0500

    20190527 Segundo Commit

--- 01-JavaHello/src/com/srccodes/example/HelloWorld.java
+++ 01-JavaHello/src/com/srccodes/example/HelloWorld.java
@@ -4,10 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello World");
-        System.out.println("How are You?");
-        System.out.println("Proof Stage");
-        System.out.println("Proof GUI and git status");
+        System.out.println("Para el 2do. Commit");
 	}
 
 }
