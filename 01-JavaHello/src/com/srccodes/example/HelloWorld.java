e70c4682caa81c07482346f1fdc778b2a83c7ba9
2006.gbm@gmail.com <GBM-2006>
2019-05-28 16:22:08 -0500

    4to Commit con "Commit and Push"

--- 01-JavaHello/src/com/srccodes/example/HelloWorld.java
+++ 01-JavaHello/src/com/srccodes/example/HelloWorld.java
@@ -4,9 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Para el 2do. Commit");
-        System.out.println("3er Commit Hello World");
-        
+        System.out.println("4to Commit Hello World");
 	}
 
 }
--- 02-JavaGit/src/HelloGit.java
+++ 02-JavaGit/src/HelloGit.java
@@ -3,9 +3,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello Git_1");
-        System.out.println("Hello Git_2");
-        System.out.println("3er Commit Hello Git_3");
+        System.out.println("4to Commit Hello Git_3");
 	}
 
 }
