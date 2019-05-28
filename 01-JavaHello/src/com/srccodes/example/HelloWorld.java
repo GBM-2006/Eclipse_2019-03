f815c62b05edcc6b82cb83ea1d1eb01c087dc230
2006.gbm@gmail.com <GBM-2006>
2019-05-28 16:01:08 -0500

    Este es el tercer Commit

--- 01-JavaHello/src/com/srccodes/example/HelloWorld.java
+++ 01-JavaHello/src/com/srccodes/example/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Para el 2do. Commit");
+        System.out.println("3er Commit Hello World");
+        
 	}
 
 }
--- 02-JavaGit/src/HelloGit.java
+++ 02-JavaGit/src/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 		// TODO Auto-generated method stub
         System.out.println("Hello Git_1");
         System.out.println("Hello Git_2");
+        System.out.println("3er Commit Hello Git_3");
 	}
 
 }
